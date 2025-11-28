7acf2736426f553c0218c240456642923c38e04f
tanyy53 <tanyy53@mail2.sysu.edu.cn>
2025-11-28 14:10:50 +0800

    Remove dataset directory from tracking and apply .gitignore

--- gen_extra.py
+++
@@ -1,5 +0,0 @@
-from dinov2.data.datasets import Pathology
-
-for split in Pathology.Split:
-    dataset = Pathology(split=split, root="./dataset/pathology-tcga-224/", extra="./dataset/pathology-tcga-224/",sshid="13")
-    dataset.dump_extra()
